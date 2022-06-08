f2ecb51f454979a7b76c939b8470b74656a83fbc
Daniel Curda <dgcurda@gmail.com>
2022-06-07 19:54:47 -0500

    more updates

--- js/main.js
+++ js/main.js
@@ -64,10 +64,15 @@ async function main() {
         let parentEl = document.createElement("div")
         document.getElementById(item[6]).appendChild(parentEl)
         parentEl.classList.add(item[1])
+        parentEl.id = item[1] + idI
+        //icon-title-button holder
+        let iconTitleButton = document.createElement("div")
+        iconTitleButton.classList.add('icon-title-button')
+        parentEl.appendChild(iconTitleButton)
         //icon-title holder
-        let iconTitle = document.createElement("div")
+        let iconTitle = document.createElement('div')
         iconTitle.classList.add('icon-title')
-        parentEl.appendChild(iconTitle)
+        iconTitleButton.appendChild(iconTitle)
         //icon
         let icon = document.createElement('img')
         icon.classList.add("item-icon")
@@ -78,19 +83,26 @@ async function main() {
         let itemName = item[2].substring(0,item[2].indexOf('{')).trim()
         titleEl.innerText = itemName
         iconTitle.appendChild(titleEl)
+        //button holder
+        let buttonHolder = document.createElement('div')
+        buttonHolder.classList.add('buttonHolder')
+        iconTitleButton.appendChild(buttonHolder)
         //expand/contract icon
         let downChevron = document.createElement('i')
         downChevron.classList.add('fa-solid', 'fa-chevron-down', 'up-down')
+        downChevron.dataset.adrucTargetElement = `#ul${item[1]}${idI}`
         downChevron.addEventListener('click',expandContract)
-        iconTitle.appendChild(downChevron)
+        buttonHolder.appendChild(downChevron)
         //add/remove icon
         let rightChevron = document.createElement('i')
         rightChevron.classList.add('fa-solid','fa-chevron-right', 'left-right')
+        rightChevron.dataset.adrucTargetElement = `#${item[1]}${idI}`
         rightChevron.addEventListener('click',addRemove)
-        iconTitle.appendChild(rightChevron)
+        buttonHolder.appendChild(rightChevron)
         //ul
         let attribEl = document.createElement('ul')
         attribEl.classList.add(item[4], "noShow")
+        attribEl.id = `ul${item[1]}${idI}`
         parentEl.appendChild(attribEl)
         //create a "name" attribute
         let nameAttrib = document.createElement('li')
@@ -120,6 +132,7 @@ async function main() {
                 liEl.appendChild(leftSpan)
                 let rightSpan = document.createElement('span')
                 rightSpan.classList.add('valuePair')
+                rightSpan.dataset.adrucTargetElement = `#${item[1]}${idI}`
                 let attribVal = attribSpl[1].trim()
                 rightSpan.innerText = attribVal
                 liEl.appendChild(rightSpan)
@@ -129,9 +142,6 @@ async function main() {
                 }
                 if (attribName == 'ResultItem') {
                     //need to get the icon from here
-                    //let tmpFlter = new RegExp(attribVal + "\\r")
-                    //console.log(tmpFlter)
-                    //console.log(`looking for: ${attribVal}`)
                     let doots = allArray.filter(function(v,i) {
                         
                         return v[2].substring(0,v[2].indexOf('{')).trim() === attribVal
@@ -139,12 +149,7 @@ async function main() {
                     //console.log(doots)
                     for (let ii = 0; ii < doots.length; ii++){
                         if (doots[ii] !== item) {
-                            //console.log(item)
-                            //console.log(doots[ii][2])
                             let thing1 = `/img/Item_${doots[ii][2].split('Icon = ')[1].substring(0,doots[ii][2].split('Icon = ')[1].indexOf(','))}.png`
-                            //test if it's there
-                            //let checker = getThings(thing1)
-                            //console.log(checker)
                             icon.src = thing1
                         }
 
@@ -170,6 +175,203 @@ async function getThings(path) {
     let theString = await theObject.text()
     return theString
 }
+
+function makeIngredientSlots(parentItem) {
+    let ingOL = document.createElement('ol')
+    ingOL.classList.add('selectedItem')
+    document.getElementById('selectedThings').appendChild(ingOL)
+    let loopVal = parentItem.querySelectorAll('.MaxItems .valuePair')[0].innerText
+    for (i = 0; i < loopVal; i++) {
+        let ingLI = document.createElement('li')
+        ingLI.classList.add('emptyOLLI')
+        ingOL.appendChild(ingLI)
+    }
+
+}
+function tallyTheThings() {
+    //tallyTheThings will happen before and after the selectedThings div is populated, so make sure sumContainer actually exists
+    let sumContainer = document.querySelectorAll(".sumContainer")[0]
+    if (!sumContainer) {
+        sumContainer = document.createElement("ul")
+        sumContainer.classList.add("sumContainer")
+        sumContainer.innerHTML = "<h3>Totals</h3>"
+        document.getElementById("selectedThings").appendChild(sumContainer)
+    }    
+    
+    tallyArray.forEach(item => {
+        let itemDatas = document.querySelectorAll(".selectedItem ." + item)
+        let sumTotal = 0
+        itemDatas.forEach(itemData => {
+            sumTotal = sumTotal + parseFloat(itemData.querySelectorAll(".valuePair")[0].innerText)
+        })
+        let checkItem = document.querySelector(".sum" + item)
+        if (!checkItem) {
+            //there isn't, create the it
+            checkItem = document.createElement("li")
+            checkItem.classList.add("sum" + item)
+            let keyPair = document.createElement("span")
+            keyPair.innerText = item
+            keyPair.classList.add("keyPair","afterColon")
+            let valuePair = document.createElement("span")
+            valuePair.classList.add("valuePair")
+            checkItem.appendChild(keyPair)
+            checkItem.appendChild(valuePair)
+            sumContainer.appendChild(checkItem)
+        }
+        document.querySelectorAll(".sum" + item + " .valuePair")[0].innerText = sumTotal
+    })
+}
+function removeIngredient() {
+
+    this.removeEventListener('click',removeIngredient)
+    this.parentElement.parentElement.classList.add('emptyOLLI')
+    this.parentElement.remove()
+    tallyTheThings()
+}
+function searchFilter() {
+    let filterItems = document.querySelectorAll(`${this.dataset.adrucFtarget}`)
+    filterItems.forEach(filterItem => {
+        let filterCompare = filterItem.querySelectorAll(`${this.dataset.adrucCompareLocation}`)[0].innerText.toLowerCase()
+        if (filterCompare.indexOf(this.value.toLowerCase()) == -1) {
+            filterItem.classList.add("filterNoShow")
+        } else {
+            filterItem.classList.remove("filterNoShow")
+        }
+    })
+}
+function expandContract() {
+    let parentItem = findTargetElement(this)
+    
+    if (this.classList.toString().includes('fa-chevron-down')) {
+        //expand
+        this.classList.remove('fa-chevron-down')
+        this.classList.add('fa-chevron-up')
+        parentItem.classList.remove('noShow')
+        //this.parentElement.parentElement.parentElement.querySelectorAll('ul')[0].classList.remove('noShow')
+    } else {
+        //contract
+        this.classList.remove('fa-chevron-up')
+        this.classList.add('fa-chevron-down')
+        parentItem.classList.add('noShow')
+        //this.parentElement.parentElement.parentElement.querySelectorAll('ul')[0].classList.add('noShow')
+    }
+}
+function addRemove() {
+    let parentItem = findTargetElement(this)
+    let chevronCheck = parentItem.querySelectorAll('.left-right')[0]
+    let whatTo = ''
+    let whatDo = ''
+    if (parentItem.classList.toString().includes('recipe')) {
+        whatTo = 'recipe'
+    } else {
+        whatTo = 'foodItem'
+    }
+    if (chevronCheck.classList.toString().includes('fa-chevron-right')) {
+        //add
+        chevronCheck.classList.remove('fa-chevron-right')
+        chevronCheck.classList.add('fa-chevron-left')
+        whatDo = 'add'
+    } else {
+        //remove
+        chevronCheck.classList.remove('fa-chevron-left')
+        chevronCheck.classList.add('fa-chevron-right')
+        whatDo = 'remove'
+    }
+    switch(whatTo) {
+        case 'recipe':
+            switch(whatDo) {
+                case 'add':
+                    let addedThing = document.getElementById('selectedThings')
+                    addedThing.appendChild(parentItem)
+                    parentItem.querySelectorAll('.up-down')[0].addEventListener('click',expandContract)
+                    parentItem.querySelectorAll('.left-right')[0].addEventListener('click',addRemove)
+                    document.getElementById('evolvedrecipe').classList.add("noShow")
+                    //let EvolvedRecipes = document.querySelectorAll('#item .EvolvedRecipe .valuePair')
+                    let allFoodItems = document.querySelectorAll('.foodItem')
+                    allFoodItems.forEach(item => {
+                        let evolvedRecipe = item.querySelectorAll('.EvolvedRecipe .valuePair')
+                        if (evolvedRecipe.length == 0) {
+                            item.classList.add('noShow')
+                        } else {
+                            /*console.log(evolvedRecipe)
+                            console.log(evolvedRecipe.innerText)*/
+
+                            if (evolvedRecipe[0].innerText.includes(`${parentItem.querySelectorAll('.recipeTitle')[0].innerText}:`)) {
+                                item.classList.remove('noShow')
+                            } else {
+                                item.classList.add('noShow')
+                            }
+                            
+                        }
+                    })
+                    /*EvolvedRecipes.forEach(item => {
+                        
+                        let parentElement = findTargetElement(item)
+                        if (item.innerText.includes(`${parentItem.querySelectorAll('.recipeTitle').innerText}:`)) {
+                            parentElement.classList.remove('noShow')
+                        } else {
+                            parentElement.classList.add('noShow')
+                        }
+                    })*/
+                    makeIngredientSlots(parentItem)
+                    tallyTheThings()
+                    break
+                case 'remove':
+                    document.getElementById('selectedThings').innerHTML = ''
+                    document.getElementById('evolvedrecipe').classList.remove('noShow')
+                    break
+            }
+            break
+        case 'foodItem':
+            switch(whatDo) {
+                case 'add':
+                    //find the first emptyOLLI
+                    let emptySlots = document.querySelectorAll('.emptyOLLI')
+                    if (emptySlots.length == 0) {
+                        alert("no empty slots - remove an ingredient first")
+                    } else {
+                        let newParent = parentItem.cloneNode(true)
+                        
+                        //update the id of the ul and the data-adruc-target-element
+                        newParent.id = `${newParent.id}_${emptySlots.length}`
+                        
+                        let upDown = newParent.querySelectorAll('.up-down')[0]
+                        upDown.dataset.adrucTargetElement = `#ul${newParent.id}`
+                        upDown.addEventListener('click',expandContract)
+                        let leftRight = newParent.querySelectorAll('.left-right')[0]
+                        leftRight.dataset.adrucTargetElement = `#${newParent.id}`
+                        leftRight.addEventListener('click',addRemove)
+                        newParent.querySelectorAll('.foodItemContents')[0].id = `ul${newParent.id}`
+                        emptySlots[0].appendChild(newParent)
+                        emptySlots[0].classList.remove('emptyOLLI')
+                        //re-add the eventlisteners
+                        tallyTheThings()
+                    }
+                    break
+                case 'remove':
+                    //set the innerHTML of the clicked item's LI to ''
+                    let parentLI = this.parentElement.parentElement.parentElement
+                    parentLI.innerHTML = ''
+                    //call tallyTheThings()
+                    tallyTheThings()
+                    //re-add emptyOLLI
+                    parentLI.classList.add('emptyOLLI')
+                    break
+            }
+            break
+    }
+}
+function findTargetElement(originalElement) {
+    let targetElements = document.querySelectorAll(originalElement.dataset.adrucTargetElement)
+    
+    if (targetElements.length == 1) {
+        targetElements = targetElements[0]
+    }
+    return targetElements
+}
+
+
+/*OLD STUFF
 function dragRStart() {
     //When dragging recipes
     document.getElementById('selectedThings').classList.add('dragLanding')
@@ -260,152 +462,4 @@ function dragLeave() {
 }
 function dragOver(e) {
     e.preventDefault()
-}
-function makeIngredientSlots(parentItem) {
-    let ingOL = document.createElement('ol')
-    ingOL.classList.add('selectedItem')
-    document.getElementById('selectedThings').appendChild(ingOL)
-    let loopVal = parentItem.querySelectorAll('.MaxItems .valuePair')[0].innerText
-    for (i = 0; i < loopVal; i++) {
-        let ingLI = document.createElement('li')
-        ingLI.classList.add('emptyOLLI')
-        ingOL.appendChild(ingLI)
-    }
-
-}
-function tallyTheThings() {
-    //tallyTheThings will happen before and after the selectedThings div is populated, so make sure sumContainer actually exists
-    let sumContainer = document.querySelectorAll(".sumContainer")[0]
-    if (!sumContainer) {
-        sumContainer = document.createElement("ul")
-        sumContainer.classList.add("sumContainer")
-        sumContainer.innerHTML = "<h3>Totals</h3>"
-        document.getElementById("selectedThings").appendChild(sumContainer)
-    }    
-    
-    tallyArray.forEach(item => {
-        let itemDatas = document.querySelectorAll(".selectedItem ." + item)
-        let sumTotal = 0
-        itemDatas.forEach(itemData => {
-            sumTotal = sumTotal + parseFloat(itemData.querySelectorAll(".valuePair")[0].innerText)
-        })
-        let checkItem = document.querySelector(".sum" + item)
-        if (!checkItem) {
-            //there isn't, create the it
-            checkItem = document.createElement("li")
-            checkItem.classList.add("sum" + item)
-            let keyPair = document.createElement("span")
-            keyPair.innerText = item
-            keyPair.classList.add("keyPair","afterColon")
-            let valuePair = document.createElement("span")
-            valuePair.classList.add("valuePair")
-            checkItem.appendChild(keyPair)
-            checkItem.appendChild(valuePair)
-            sumContainer.appendChild(checkItem)
-        }
-        document.querySelectorAll(".sum" + item + " .valuePair")[0].innerText = sumTotal
-    })
-}
-function removeIngredient() {
-
-    this.removeEventListener('click',removeIngredient)
-    this.parentElement.parentElement.classList.add('emptyOLLI')
-    this.parentElement.remove()
-    tallyTheThings()
-}
-function searchFilter() {
-    let filterItems = document.querySelectorAll(`${this.dataset.adrucFtarget}`)
-    filterItems.forEach(filterItem => {
-        let filterCompare = filterItem.querySelectorAll(`${this.dataset.adrucCompareLocation}`)[0].innerText.toLowerCase()
-        if (filterCompare.indexOf(this.value.toLowerCase()) == -1) {
-            filterItem.classList.add("noShow")
-        } else {
-            filterItem.classList.remove("noShow")
-        }
-    })
-}
-function expandContract() {
-    if (this.classList.toString().includes('fa-chevron-down')) {
-        //expand
-        this.classList.remove('fa-chevron-down')
-        this.classList.add('fa-chevron-up')
-        this.parentElement.parentElement.querySelectorAll('ul')[0].classList.remove('noShow')
-    } else {
-        //contract
-        this.classList.remove('fa-chevron-up')
-        this.classList.add('fa-chevron-down')
-        this.parentElement.parentElement.querySelectorAll('ul')[0].classList.add('noShow')
-    }
-}
-function addRemove() {
-    //determine if this is a recipe or foodItem
-    let parentItem = this.parentElement.parentElement.cloneNode(true)
-    let chevronCheck = parentItem.querySelectorAll('.left-right')[0]
-    let whatTo = ''
-    let whatDo = ''
-    if (parentItem.classList.toString().includes('recipe')) {
-        whatTo = 'recipe'
-    } else {
-        whatTo = 'foodItem'
-    }
-    if (chevronCheck.classList.toString().includes('fa-chevron-right')) {
-        //add
-        chevronCheck.classList.remove('fa-chevron-right')
-        chevronCheck.classList.add('fa-chevron-left')
-        whatDo = 'add'
-    } else {
-        //remove
-        chevronCheck.classList.remove('fa-chevron-left')
-        chevronCheck.classList.add('fa-chevron-right')
-        whatDo = 'remove'
-    }
-    switch(whatTo) {
-        case 'recipe':
-            switch(whatDo) {
-                case 'add':
-                    //let addedThing = document.getElementById('selectedThings').appendChild(parentItem)
-                    let addedThing = document.getElementById('selectedThings')
-                    addedThing.appendChild(parentItem)
-                    parentItem.querySelectorAll('.up-down')[0].addEventListener('click',expandContract)
-                    parentItem.querySelectorAll('.left-right')[0].addEventListener('click',addRemove)
-                    document.getElementById('evolvedrecipe').classList.add("noShow")
-                    makeIngredientSlots(parentItem)
-                    tallyTheThings()
-                    break
-                case 'remove':
-                    document.getElementById('selectedThings').innerHTML = ''
-                    document.getElementById('evolvedrecipe').classList.remove('noShow')
-                    break
-            }
-            break
-        case 'foodItem':
-            switch(whatDo) {
-                case 'add':
-                    //find the first emptyOLLI
-                    let emptySlots = document.querySelectorAll('.emptyOLLI')
-                    if (emptySlots.length == 0) {
-                        alert("no empty slots - remove an ingredient first")
-                    } else {
-                        let newParent = parentItem.cloneNode(true)
-                        //console.log(newParent)
-                        newParent.querySelectorAll('.up-down')[0].addEventListener('click',expandContract)
-                        newParent.querySelectorAll('.left-right')[0].addEventListener('click',addRemove)
-                        emptySlots[0].appendChild(newParent)
-                        emptySlots[0].classList.remove('emptyOLLI')
-                        //re-add the eventlisteners
-                        tallyTheThings()
-                    }
-                    break
-                case 'remove':
-                    //set the innerHTML of the clicked item's LI to ''
-                    let parentLI = this.parentElement.parentElement.parentElement
-                    parentLI.innerHTML = ''
-                    //call tallyTheThings()
-                    tallyTheThings()
-                    //re-add emptyOLLI
-                    parentLI.classList.add('emptyOLLI')
-                    break
-            }
-            break
-    }
-}
+}*/
